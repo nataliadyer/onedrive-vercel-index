18e7ee8be385f71bf9a3e948a55f9f10b8401146
Natalia Dyer <nataliadyer@premiumnulledthemes.com>
2024-06-18 08:52:30 +0530

    index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,8 @@ export default function Home() {
         <meta name="robots" content="noindex, nofollow" />
         <script type='text/javascript' src='//sortexampleposition.com/2d/fc/c4/2dfcc4499eb2fd87ac12369e6d9da638.js' />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-RBCGSFJ4CF" />
-      </Head>
+        
+        <script type="text/javascript" data-cfasync="false" src="../components/Customjs.js"/>
 
       <main className="flex w-full flex-1 flex-col bg-gray-50 dark:bg-gray-800">
         <Navbar />
